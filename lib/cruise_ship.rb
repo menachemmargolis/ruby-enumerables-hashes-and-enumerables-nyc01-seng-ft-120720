38a15077476cafec3dc8b6d79b13f220c02d1bb6
menachem m margolis <margolismendel5728@gmail.com>
2020-11-30 18:00:47 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,14 +1,19 @@
 # passengers = {
-# suite_a: "Amanda Presley",
-# suite_b: "Seymour Hoffman",
-# suite_c: "Alfred Tennyson",
-# suite_d: "Charlie Chaplin",
+# suite_a: "Amanda Presley", 
+# suite_b: "Seymour Hoffman", 
+# suite_c: "Alfred Tennyson", 
+# suite_d: "Charlie Chaplin", 
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
   # add the code snippet here!
-  winner =""
-passengers.each do|a,b| if a == :suite_a && b.start_with?("A") winner = b end  end
-
+  winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ 
+winner
 end
